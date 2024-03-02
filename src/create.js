310139220fd4684f54069585e066d26fd8ef8d39
Artur Todeschni Crestani <artodeschini@gmail.com>
2024-03-01 23:46:20 -0300

    corrige e tras correcoes da nova versao

--- src/create.js
+++ src/create.js
@@ -1,15 +1,34 @@
 import { displayLog } from './utils';
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
+
 
 export default () => {
     /** start coding */
-    const hello = Observable.create(function(observer) {
+    // new Observable((observer: Observer) => {
+    //     observer.next();
+    //     observer.complete();
+    // });
+    // const hello = Observable.create(function(observer) {
+    //const hello = new Observable((observer: Observer<string>) => {        
+    const hello = new Observable((observer) => {
         observer.next('Hello');
         setTimeout(()=>{
             observer.next('World');
+            observer.complete();
         }, 2000);
+
+        // observer.complete(); nunca ira escrever o segundo aqui
     });
+
+    const observer = {
+        next: e => displayLog(e),
+        error: err => console.error(`[Erro] ${err}`),
+        complete: () => displayLog("Completou!!!")
+    }
     
-    const subscribe = hello.subscribe(evt => displayLog(evt));
+    const subscribe1 = hello.subscribe(observer);// evt => displayLog(evt));
+    const subscribe2 = hello.subscribe(observer);// evt => displayLog(evt));
+    // remove a subscricao cancelando as d+ execucoes dos proximos
+    subscribe1.unsubscribe();
     /** end coding */
 }
