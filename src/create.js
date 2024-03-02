69874b28e7cbf26ed90ef7910f13448644511f9d
Artur Todeschni Crestani <artodeschini@gmail.com>
2024-03-02 00:04:09 -0300

    resolvendo para funcionar nas versoes novas

--- src/create.js
+++ src/create.js
@@ -1,7 +1,21 @@
 import { displayLog } from './utils';
+import { from } from 'rxjs';
 
 export default () => {
     /** start coding */
+    const myArray = [0,1,2,3,4,5,6,7];
+    const myString = "Hello World";
+    const myPromise = new Promise(resolve => setTimeout(() => {
+        resolve("Minha Promise!")
+    }, 2000));
+
+    const observable1 = from(myArray);
+    const observable2 = from(myString);
+    const observable3 = from(myPromise);
+
+    const subscribe1 = observable1.subscribe(v => displayLog(v));
+    const subscribe2 = observable2.subscribe(v => displayLog(v));
+    const subscribe3 = observable3.subscribe(v => displayLog(v));
 
     /** end coding */
 }
