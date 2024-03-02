15c684fce309d9ed547a0b074e1b02cc61186736
Artur Todeschni Crestani <artodeschini@gmail.com>
2024-03-02 20:57:03 -0300

    correcoes para versoes atuais

--- src/create.js
+++ src/create.js
@@ -1,9 +1,18 @@
 import { displayLog } from './utils';
+import { interval, timer } from 'rxjs';
 
 export default () => {
     /** start coding */
+    const source1 = interval(500);
+    const subscription1 = source1.subscribe(v => displayLog(v));
 
+    // setTimeout(() => subscription.unsubscribe(), 3000);
+    timer(3000).subscribe(() => subscription1.unsubscribe());
 
+    const source2 = timer(4000, 100);
+    const subscription2 = source2.subscribe(v => displayLog(`2 -- ${v}`));
+    
+    timer(6000).subscribe(() => subscription2.unsubscribe());
 
     /** end coding */
 }
