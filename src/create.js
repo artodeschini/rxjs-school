a959320527cd8a2f32c092e4111e7002ec1e9b03
Artur Todeschni Crestani <artodeschini@gmail.com>
2024-03-02 21:16:22 -0300

    ajustes para versao atual

--- src/create.js
+++ src/create.js
@@ -1,9 +1,21 @@
 import { displayLog } from './utils';
+import { fromEvent } from 'rxjs';
 
 export default () => {
     /** start coding */
+    const acaoBotao = document.getElementById("action-btn");
 
+    const source1 = fromEvent(acaoBotao, "click");
 
+    source1.subscribe(event => {
+        displayLog(`click event at posição (${event.x}, ${event.y})`);
+    });
+
+    const source2 = fromEvent(document, "mousemove");
+
+    source2.subscribe(event => {
+        console.log(event);
+    });
 
     /** end coding */
 }
