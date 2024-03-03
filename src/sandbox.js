3dac77e0001a1a545bb3906e074ffe9895a5cd01
Artur Todeschni Crestani <artodeschini@gmail.com>
2024-03-02 22:24:19 -0300

    corrigindo para versoes atuais operador tap

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,15 +1,18 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
     const grid = document.getElementById('grid');
     const click$ = fromEvent(grid, 'click').pipe(
-        map(val => [ 
+        tap(antes => console.log(`antes original ${antes} e ${JSON.stringify(antes)}`)),
+        map(val => [
+            // pega as coordenadas ma grid cada celula tem 50px x 50px
             Math.floor(val.offsetX/50), 
             Math.floor(val.offsetY/50)
-        ])
+        ]),
+        tap(depois => console.log(`depois ${depois}`))
     );
 
     const subscription = click$.subscribe(data => displayLog(data));
