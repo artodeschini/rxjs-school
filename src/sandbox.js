f1b3baebc7f723d1f566b09e13156f64369328b7
Artur Todeschni Crestani <artodeschini@gmail.com>
2024-03-13 22:28:39 -0300

    ataulizado codigo com voersoes nao obsoletas

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, takeWhile } from 'rxjs/operators';
+import { map, takeWhile, last, tap, takeLast, skip } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -10,7 +10,14 @@ export default () => {
             Math.floor(val.offsetX/50), 
             Math.floor(val.offsetY/50)
         ]),
-        takeWhile( ([col, row]) => col > 3 )
+        // takeWhile( ([col, row]) => col > 3 ),
+        // tap(v => console.log(`${v} é valido em operador takeWhile`)),
+        // ultimo evento
+        // last()
+        // ultimo n envetos
+        // takeLast(3) // tres eventos
+        tap(v => console.log(`${v} coordenada`)),
+        skip(5) // pula 5
     );
 
     const subscription = click$.subscribe(data => displayLog(data));
